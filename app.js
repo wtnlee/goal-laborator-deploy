bd8ff891cc6eab683ff5823819bff4196e8fa7ea
wtnlee <wtnlee@seas.upenn.edu>
2020-04-21 00:12:22 -0700

    kill

--- app.js
+++ app.js
@@ -44,9 +44,9 @@ app.use(bodyParser.json());
 app.use(express.json());       // to support JSON-encoded bodies
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://goalclient.herokuapp.com');
   res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type' , 'Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type' );
   res.header( "Access-Control-Allow-Credentials", true );
   
   if (req.method === 'OPTIONS') {
